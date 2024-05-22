1bb523045f6c1696e53eba52348b75e419908e17
Полина Орлова <cantatcaelum@yandex.ru>
2024-05-22 16:58:27 +0300

    fix: правка обновления пользователя

--- src/main/java/ru/practicum/shareit/user/InMemoryUserRepository.java
+++ src/main/java/ru/practicum/shareit/user/InMemoryUserRepository.java
@@ -2,10 +2,12 @@ package ru.practicum.shareit.user;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import ru.practicum.shareit.exceptions.DataNotFoundException;
 import ru.practicum.shareit.user.model.User;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Primary
@@ -37,8 +39,37 @@ public class InMemoryUserRepository implements UserRepository {
 
     @Override
     public User updateUser(long id, User user) {
-        users.put(user.getId(), user);
-        return user;
+        if (users.containsKey(id)) {
+            if (users.get(id).getEmail().equals(user.getEmail())) {
+                if (user.getName() != null) {
+                    users.get(id).setName(user.getName());
+                }
+            } else {
+                if (!checkIfExists(user)) {
+                    if (user.getName() != null)
+                        users.get(id).setName(user.getName());
+                    if (user.getEmail() != null)
+                        users.get(id).setEmail(user.getEmail());
+                } else {
+                    throw new DataNotFoundException(
+                            String.format("Ошибка. Пользователь с адресом почты = %s не найден.", user.getEmail()));
+                }
+            }
+            return users.get(id);
+        } else {
+            throw new DataNotFoundException(String.format("Ошибка. Пользователь с id = %d не найден.", id));
+        }
+    }
+
+    private boolean checkIfExists(User user) {
+        if (user.getEmail() == null)
+            return false;
+        List<String> existingEmails = users.values().stream().map(u -> u.getEmail()).collect(Collectors.toList());
+        for (String email : existingEmails) {
+            if (user.getEmail().equals(email))
+                return true;
+        }
+        return false;
     }
 
     @Override
